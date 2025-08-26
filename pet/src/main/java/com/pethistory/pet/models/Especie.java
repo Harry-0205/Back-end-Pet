8b4b9e6a1ba43a4c568af2c36d9aac08dea11168
Alejo <lhasdjias>
2025-08-26 17:13:59 -0500

    tablas fuertes

---
+++ pet/src/main/java/com/pethistory/pet/models/Especie.java
@@ -0,0 +1,22 @@
+package com.pethistory.pet.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name="Especie")
+public class Especie {
+    @Id
+    @Column(name = "idEspecie", length = 11)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idEspecie;
+    private String especie;
+
+
+}
