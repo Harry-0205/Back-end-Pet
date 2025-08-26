2f8b647214dc0840b7b5c67ad7e539972ebea8af
carlosbue3 <141794660+carlosbue3@users.noreply.github.com>
2025-08-26 16:53:34 -0500

    dtosvetehist

--- pet/src/main/java/com/pethistory/pet/dtos/HistoriaClinicaDto.java
+++ pet/src/main/java/com/pethistory/pet/dtos/HistoriaClinicaDto.java
@@ -1,5 +1,16 @@
 package com.pethistory.pet.dtos;
 
-public class HistoriaClinicaDto {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+
+public class HistoriaClinicaDto {
+    private Long idHist;
+    private Long idpet;
+    private Long idproce;
+    private String anot;
 }
