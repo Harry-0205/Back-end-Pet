1c1ccde8f23479b2e8daf61ae3759d7ad52e0c80
Photobogotanos <Photobogotanos@users.noreply.github.com>
2025-08-28 15:52:09 -0500

    Modificaciones Modulo Usu

--- pet/src/main/java/com/pethistory/pet/models/HistoriaClinica.java
+++ pet/src/main/java/com/pethistory/pet/models/HistoriaClinica.java
@@ -1,16 +1,10 @@
 package com.pethistory.pet.models;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,6 +17,5 @@ public class HistoriaClinica {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idHistoria;
-    @Column( length = 200)
     private String anotaciones;
 }
