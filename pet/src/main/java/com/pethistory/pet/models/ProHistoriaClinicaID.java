2884b427c45e83554974e7ff6c46a34532059f65
Jossimar10 <Jossimartinez0912@gmail.com>
2025-09-03 14:51:40 -0500

    Conexion carlos

---
+++ pet/src/main/java/com/pethistory/pet/models/ProHistoriaClinicaID.java
@@ -0,0 +1,22 @@
+package com.pethistory.pet.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"idHistoria","idProcedimiento"})
+public class ProHistoriaClinicaID implements Serializable{
+    public ProHistoriaClinicaID( Integer id1, Integer id2){
+    }
+    private Integer idHistoria;
+    private Long idProcedimiento;
+
+}
