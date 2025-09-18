a388b60c7f10e41fe774e7f4ad47e0c184784fb4
Harry <sanchezmormiguel815@gmail.com>
2025-09-18 16:59:52 -0500

    Carlos: continuacion implementacion Veterinas

--- pet/src/main/java/com/pethistory/pet/mapper/VeterinariaMapperImplement.java
+++ pet/src/main/java/com/pethistory/pet/mapper/VeterinariaMapperImplement.java
@@ -2,7 +2,29 @@ package com.pethistory.pet.mapper;
 
 import org.springframework.stereotype.Component;
 
-@Component
-public class VeterinariaMapperImplement {
+import com.pethistory.pet.dtos.VeterinariasDto;
+import com.pethistory.pet.models.Veterinarias;
 
+@Component
+public class VeterinariaMapperImplement implements VeterinariaMapper{
+
+@Override
+public Veterinarias toVeterinarias(VeterinariasDto veterinariasDto){
+    if (veterinariasDto == null) {
+        return null;
+        
+    }
+    Veterinarias vete =new Veterinarias();
+    vete.setIdVet(veterinariasDto.getIdVeterinaria());
+    vete.setNom(veterinariasDto.getNombre());
+    vete.setCorreo(veterinariasDto.getMail());
+    vete.setFoto(veterinariasDto.getFotico());
+    vete.setDirec(veterinariasDto.getDireccion());
+
+    return vete;
+
+    }
+    @Override
+    public VeterinariasDto 
+    
 }
