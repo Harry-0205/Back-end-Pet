7a9d102a3b54440087cc50db2821a151fbe321dd
Caliche <carlochax651@gmail.com>
2025-09-19 15:36:09 -0500

    realizar serviceimple Carlos

--- pet/src/main/java/com/pethistory/pet/service/VeterinariasServiceImple.java
+++ pet/src/main/java/com/pethistory/pet/service/VeterinariasServiceImple.java
@@ -7,19 +7,36 @@ import com.pethistory.pet.mapper.VeterinariaMapper;
 import com.pethistory.pet.models.Veterinarias;
 import com.pethistory.pet.repositories.VeterinariasRepositories;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class VeterinariasServiceImple implements VeterinariasService{
 private final VeterinariasRepositories veterep;
 private final VeterinariaMapper vetemap;
 
 public VeterinariasServiceImple(VeterinariasRepositories veterep, VeterinariaMapper vetemap){
-    this.veterep;
-    this.vetemap;
+    this.veterep=veterep;
+    this.vetemap=vetemap;
     
 }
 @Override
 public VeterinariasDto crear(VeterinariasDto veterinariasDto){
-    Veterinarias veterinarias=vetemap.toVeterinarias(veterinariasDto)
+    Veterinarias veterinarias=vetemap.toVeterinarias(veterinariasDto);
+    Veterinarias guardar = veterep.save(veterinarias);
+    return vetemap.toVeterinariasDto(guardar);
+}
+@Override
+public VeterinariasDto niit(Long id){
+    return veterep.findById(id).map(vetemap::toVeterinariasDto).orElseThrow(()->new EntityNotFoundException("veterinaria no encotrada"));
+
+}
+@Override
+public VeterinariasDto cambiar(VeterinariasDto veterinariasDto){
+    Veterinarias veterinarias=veterep.findById(veterinariasDto.getIdVeterinaria())
+    .orElseThrow(()-> new EntityNotFoundException("Veterinaria no fue posible acrualizar"));
+    
+    Veterinarias actualizada=veterep.save(veterinarias);
+    return vetemap.toVeterinariasDto((actualizada));
 }
 
 }
